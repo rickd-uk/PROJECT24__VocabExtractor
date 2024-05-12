0aaf55b987abac00142e24b1299da9999ba05a77
Rick D <dev.bites.uk@gmail.com>
2024-05-12 14:05:31 +0900

    add: lists uncommon words

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
+function loadWordsFromFile(filePath) {
+  return fetch(filePath)
+    .then((res) => res.text())
+    .then((text) => text.split(/\s+/));
+}
+
+let studyList = [];
+
 function extractVocabulary() {
   const inputText = document.getElementById("textInput").value;
-  const stopWords = [
-    "a",
-    "an",
-    "the",
-    "and",
-    "or",
-    "but",
-    "is",
-    "are",
-    "was",
-    "were" /* add more stop words */,
-  ];
-  const commonWords = ["common", "word", "list" /* add more common words */];
-  const wordList = inputText
-    .replace(/[^a-zA-Z\s]/g, "")
-    .toLowerCase()
-    .split(/\s+/);
-  const uncommonWords = wordList.filter(
-    (word) =>
-      !stopWords.includes(word) &&
-      !commonWords.includes(word) &&
-      word.length > 3,
-  );
-  const outputDiv = document.getElementById("outputDiv");
-  outputDiv.innerHTML = "";
+  const stopWordsFile = "stop_words.txt";
+  const commonWordsFile = "common_words.txt";
+  const knownWordsFile = "known_words.txt";
 
-  if (uncommonWords.length === 0) {
-    outputDiv.innerHTML = "<p>No uncommon words found.</p>";
+  Promise.all([
+    loadWordsFromFile(stopWordsFile),
+    loadWordsFromFile(commonWordsFile),
+    loadWordsFromFile(knownWordsFile),
+  ])
+    .then(([stopWords, commonWords, knownWords]) => {
+      const wordList = inputText
+        .replace(/[^a-zA-Z\s]/g, "")
+        .toLowerCase()
+        .split(/\s+/);
+
+      const uncommonWords = wordList.filter(
+        (word) =>
+          !stopWords.includes(word) &&
+          !commonWords.includes(word) &&
+          !knownWords.includes(word) &&
+          word.length > 3,
+      );
+
+      const outputDiv = document.getElementById("outputDiv");
+      outputDiv.innerHTML = "";
+
+      if (uncommonWords.length == 0) {
+        outputDiv.innerHTML = "<p>No uncommon words were found.</p>";
+      } else {
+        studyList = [];
+        outputDiv.innerHTML =
+          "<h2>Uncommon Words:</h2><ul>" +
+          uncommonWords
+            .map(
+              (word) =>
+                `<li class="word" onclick="toggleStudyList('${word}')">${word}</li>`,
+            )
+            .join("") +
+          "</ul>";
+        document.getElementById("saveStudyListBtn").disabled = false;
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+function toggleStudyList(word) {
+  if (studyList.includes(word)) {
+    studyList = studyList.filter((w) => w !== word);
   } else {
-    outputDiv.innerHTML =
-      "<h2>Uncommon Words:</h2><ul>" +
-      uncommonWords.map((word) => `<li>${word}</li>`).join("") +
-      "</ul>";
+    studyList.push(word);
   }
+}
+
+function saveStudyList() {
+  const studyListDiv = document.getElementById("studyListDiv");
+  studyListDiv.innerHTML =
+    "<h2>Study List: </h2><ul>" +
+    studyList.map((word) => `<li>${word}</li>`).join("") +
+    "</ul>";
 }
